6fa84064d571807d1feb38c6c58bd9ff4cea8635
standemdem <stan.demontmarin@gmail.com>
2021-12-08 14:18:36 +0100

    init

---
+++ market.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items_list = [
+        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    ]
+    columns = items_list[0].keys()
+    return render_template('market.html', items_list=items_list, columns=columns)
