84272b6cf238cfd2605a74546dd21c4c19c5dfd8
standemdem <stan.demontmarin@gmail.com>
2021-12-09 16:49:47 +0100

    added dynamic visualisation of market db

--- market.py
+++ market.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-
 db = SQLAlchemy(app)
 
 class Item(db.Model):
@@ -11,11 +9,10 @@ class Item(db.Model):
     name = db.Column(db.String(length=30), nullable=False, unique=True)
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1824), nullable=False)
- 
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
     def __repr__(self):
         return f'Item {self.name}'
-
 
 
 @app.route('/')
@@ -25,16 +22,5 @@ def home_page():
 
 @app.route('/market_page')
 def market_page():
-    print(app.config)
-    items_list = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboar', 'barcode': '231985128446', 'price': 150}
-    ]
-    columns = items_list[0].keys()
-    return render_template('market.html', items_list=items_list, columns=columns)
-
-if __name__ == '__main__':
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    items = Item.query.all()
+    return render_template('market.html', items=items)
