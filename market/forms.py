47b1874c4ed6f3ec618a98080b85463542e99251
standemdem <stan.demontmarin@gmail.com>
2021-12-11 16:47:49 +0100

    created register form model and html page

---
+++ market/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.fields.simple import EmailField, PasswordField, SubmitField
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(label='User Name')
+    email = EmailField(label='Email')
+    password1 = PasswordField(label='Enter password')
+    password2 = PasswordField(label='Confirm password')
+    Submit = SubmitField(label='Submit')
