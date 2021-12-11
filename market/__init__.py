84218869ec100b417b637526e2c1a25c576ff2f3
standemdem <stan.demontmarin@gmail.com>
2021-12-11 17:04:03 +0100

    fixed db URI to fetch the db in the right folder

--- market/__init__.py
+++ market/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/standm/dev/marketplace/market/market.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY']='1f5f44d9fbe3bc914dc0f38c'
 from market import routes
