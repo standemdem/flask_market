4cca7f82ab6ca7c39bf395f0c75dae47c1d78991
standemdem <stan.demontmarin@gmail.com>
2021-12-17 15:18:44 +0100

    added function to hash passwords before committing to db

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template
+import bcrypt
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/standm/dev/marketplace/market/market.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY']='1f5f44d9fbe3bc914dc0f38c'
+bcrypt = Bcrypt(app)
+
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -23,7 +23,7 @@ def register_page():
     if form.validate_on_submit():
         user_to_create = User(username = form.username.data,
                                 email = form.email.data,
-                                password_hash = form.password1.data)
+                                password = form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
@@ -33,3 +33,7 @@ def register_page():
         for err_msge in form.errors.values():
             flash(f'There was an error when creating a user{err_msge}')
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET','POST'])
+def login_page():
+    return render_template('login.html')
