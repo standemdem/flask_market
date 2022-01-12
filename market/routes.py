2a8b25b2c39887718956d1f2fac7a83ef5b22501
standemdem <stan.demontmarin@gmail.com>
2022-01-12 16:59:51 +0100

    cleaned error msges

--- market/routes.py
+++ market/routes.py
@@ -38,17 +38,15 @@ def register_page():
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
     form = LoginForm()
-    print('-----------------------test')
     if form.validate_on_submit():
-        print('isit HERE ???????????????')
         attempted_user = User.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
             login_user(attempted_user)
             flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
-            print('-----------------------test')
+            print(f'{attempted_user.username} has logged in')
             return redirect(url_for('market_page'))
         else:
-            print('-----------------------test')
+            print('wrong login or password')
             flash('Username and password are not matched! Please try again', category='danger')
 
     return render_template('login.html', form=form)
